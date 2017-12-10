007747d2915aecdeb62f6dbec02069b6ac0dd951
Nick Nicholas <nicknicholas@ICTs-MacBook.local>
2017-12-10 18:05:54 +1100

    init

---
+++ lib/asciidoctor-iso.rb
@@ -0,0 +1,3 @@
+require "asciidoctor" unless defined? Asciidoctor::Converter
+require_relative "asciidoctor/iso/converter"
+require_relative "asciidoctor/iso/version"
---
+++ lib/asciidoctor/iso/converter.rb
@@ -0,0 +1,47 @@
+require "asciidoctor"
+
+require "asciidoctor/iso/version"
+require "asciidoctor/iso/base"
+require "asciidoctor/iso/front"
+require "asciidoctor/iso/validate"
+
+module Asciidoctor
+  module ISO
+    # A {Converter} implementation that generates ISO output, and a document
+    # schema encapsulation of the document for validation
+    class Converter
+      include ::Asciidoctor::Converter
+      include ::Asciidoctor::Writer
+
+      include ::Asciidoctor::RFC::Base
+      include ::Asciidoctor::RFC::Front
+      include ::Asciidoctor::RFC::Validate
+
+      register_for "iso"
+
+      $seen_back_matter = false
+      $xreftext = {}
+
+      def initialize(backend, opts)
+        super
+        basebackend "html"
+        outfilesuffix ".xml"
+      end
+
+      # alias_method :pass, :content
+      alias_method :embedded, :content
+      alias_method :sidebar, :content
+      alias_method :audio, :skip
+      alias_method :colist, :skip
+      alias_method :page_break, :skip
+      alias_method :thematic_break, :skip
+      alias_method :video, :skip
+      alias_method :inline_button, :skip
+      alias_method :inline_kbd, :skip
+      alias_method :inline_menu, :skip
+      alias_method :inline_image, :skip
+
+      alias_method :quote, :paragraph
+    end
+  end
+end
