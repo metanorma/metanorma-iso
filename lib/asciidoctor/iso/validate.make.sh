545d10bb39a135d80a48e273a98e9c4bca12ac27
Nick Nicholas <opoudjis@optusnet.com.au>
2018-02-14 08:50:34 +1100

    Redundant file

--- lib/asciidoctor/iso/validate.make.sh
+++
@@ -1,8 +0,0 @@
-if [[ ! -d jing-trang ]]; then
-  git clone https://github.com/relaxng/jing-trang.git
-fi
-cd jing-trang
-./ant
-cd ..
-java -jar jing-trang/build/trang.jar -I rnc -O rng validate.rnc validate.rng
-
