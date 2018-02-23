09d055b05a0737377af372974766271c02679f48
Nick Nicholas <opoudjis@optusnet.com.au>
2018-02-23 22:46:16 +1100

    Section rspec; closes #102

--- spec/asciidoctor-iso/section_spec.rb
+++ spec/asciidoctor-iso/section_spec.rb
@@ -24,11 +24,13 @@ RSpec.describe Asciidoctor::ISO do
 
       == Terms and Definitions
 
-      Text
+      === Term1
 
       == Terms, Definitions, Symbols and Abbreviated Terms
 
       === Normal Terms
+
+      ==== Term2
 
       === Symbols and Abbreviated Terms
 
@@ -46,6 +48,8 @@ RSpec.describe Asciidoctor::ISO do
       === Annex A.1
 
       == Bibliography
+
+      === Bibliography Subsection
     INPUT
            <?xml version="1.0" encoding="UTF-8"?>
        <iso-standard xmlns="http://riboseinc.com/isoxml">
@@ -102,11 +106,16 @@ RSpec.describe Asciidoctor::ISO do
 
        <terms id="_" obligation="normative">
          <title>Terms and Definitions</title>
-
-       </terms>
-       <terms id="_" obligation="normative"><title>Terms, Definitions, Symbols and Abbreviated Terms</title><term id="_">
-         <preferred>Normal Terms</preferred>
+         <term id="_">
+         <preferred>Term1</preferred>
        </term>
+       </terms>
+       <terms id="_" obligation="normative"><title>Terms, Definitions, Symbols and Abbreviated Terms</title><terms id="_" obligation="normative">
+         <title>Normal Terms</title>
+         <term id="_">
+         <preferred>Term2</preferred>
+       </term>
+       </terms>
        <symbols-abbrevs id="_"/></terms>
        <symbols-abbrevs id="_"/>
        <clause id="_" obligation="normative"><title>Clause 4</title><subsection id="_" obligation="normative">
@@ -125,6 +134,9 @@ RSpec.describe Asciidoctor::ISO do
          <title>Normative References</title>
        </references><references id="_" obligation="informative">
          <title>Bibliography</title>
+         <references id="_" obligation="informative">
+         <title>Bibliography Subsection</title>
+       </references>
        </references>
        </iso-standard>
     OUTPUT
@@ -140,13 +152,15 @@ RSpec.describe Asciidoctor::ISO do
       [obligation=informative]
       == Clause 1
 
+      === Clause 1a
+
       [obligation=normative]
       == Clause 2
 
       [appendix,obligation=informative]
       == Annex
      INPUT
-       <?xml version="1.0" encoding="UTF-8"?>
+            <?xml version="1.0" encoding="UTF-8"?>
        <iso-standard xmlns="http://riboseinc.com/isoxml">
        <bibdata type="article">
          <title>
@@ -192,6 +206,9 @@ RSpec.describe Asciidoctor::ISO do
        </bibdata><version/>
        <sections><clause id="_" obligation="informative">
          <title>Clause 1</title>
+         <subsection id="_" obligation="informative">
+         <title>Clause 1a</title>
+       </subsection>
        </clause>
        <clause id="_" obligation="normative">
          <title>Clause 2</title>
@@ -202,5 +219,217 @@ RSpec.describe Asciidoctor::ISO do
        </iso-standard>
      OUTPUT
   end
+
+    it "processes inline headers" do
+     expect(strip_guid(Asciidoctor.convert(<<~'INPUT', backend: :iso, header_footer: true))).to be_equivalent_to <<~'OUTPUT'
+      = Document title
+      Author
+      :docfile: test.adoc
+      :nodoc:
+
+      == Clause 1
+
+      [%inline-header]
+      === Clause 1a
+
+      [appendix]
+      == Annex A
+
+      [%inline-header]
+      === Clause Aa
+     INPUT
+            <?xml version="1.0" encoding="UTF-8"?>
+       <iso-standard xmlns="http://riboseinc.com/isoxml">
+       <bibdata type="article">
+         <title>
+           <title-main language="en" format="text/plain"/>
+         </title>
+         <title>
+           <title-main language="fr" format="text/plain"/>
+         </title>
+         <docidentifier>
+           <project-number/>
+         </docidentifier>
+         <contributor>
+           <role type="author"/>
+           <organization>
+             <name>ISO</name>
+           </organization>
+         </contributor>
+         <contributor>
+           <role type="publisher"/>
+           <organization>
+             <name>ISO</name>
+           </organization>
+         </contributor>
+         <language/>
+         <script>Latn</script>
+         <status>
+           <stage>60</stage>
+           <substage>60</substage>
+         </status>
+         <copyright>
+           <from>2018</from>
+           <owner>
+             <organization>
+               <name>ISO</name>
+             </organization>
+           </owner>
+         </copyright>
+         <editorialgroup>
+           <technical-committee/>
+           <subcommittee/>
+           <workgroup/>
+         </editorialgroup>
+       </bibdata><version/>
+       <sections><clause id="_" obligation="normative">
+         <title>Clause 1</title>
+         <subsection id="_" inline-header="true" obligation="normative">
+         <title>Clause 1a</title>
+       </subsection>
+       </clause>
+       </sections><annex id="_" obligation="normative">
+         <title>Annex A</title>
+         <subsection id="_" inline-header="true" obligation="normative">
+         <title>Clause Aa</title>
+       </subsection>
+       </annex>
+       </iso-standard>
+     OUTPUT
+    end
+
+  it "processes blank headers" do
+     expect(strip_guid(Asciidoctor.convert(<<~'INPUT', backend: :iso, header_footer: true))).to be_equivalent_to <<~'OUTPUT'
+      = Document title
+      Author
+      :docfile: test.adoc
+      :nodoc:
+
+      == Clause 1
+
+      === {blank}
+
+     INPUT
+            <?xml version="1.0" encoding="UTF-8"?>
+       <iso-standard xmlns="http://riboseinc.com/isoxml">
+       <bibdata type="article">
+         <title>
+           <title-main language="en" format="text/plain"/>
+         </title>
+         <title>
+           <title-main language="fr" format="text/plain"/>
+         </title>
+         <docidentifier>
+           <project-number/>
+         </docidentifier>
+         <contributor>
+           <role type="author"/>
+           <organization>
+             <name>ISO</name>
+           </organization>
+         </contributor>
+         <contributor>
+           <role type="publisher"/>
+           <organization>
+             <name>ISO</name>
+           </organization>
+         </contributor>
+         <language/>
+         <script>Latn</script>
+         <status>
+           <stage>60</stage>
+           <substage>60</substage>
+         </status>
+         <copyright>
+           <from>2018</from>
+           <owner>
+             <organization>
+               <name>ISO</name>
+             </organization>
+           </owner>
+         </copyright>
+         <editorialgroup>
+           <technical-committee/>
+           <subcommittee/>
+           <workgroup/>
+         </editorialgroup>
+       </bibdata><version/>
+       <sections>
+         <clause id="_" obligation="normative">
+         <title>Clause 1</title>
+         <subsection id="_" obligation="normative">
+         <title/>
+       </subsection>
+       </clause>
+       </sections>
+       </iso-standard>
+     OUTPUT
+  end
+
+    it "processes term document sources" do
+     expect(strip_guid(Asciidoctor.convert(<<~'INPUT', backend: :iso, header_footer: true))).to be_equivalent_to <<~'OUTPUT'
+      = Document title
+      Author
+      :docfile: test.adoc
+      :nodoc:
+
+      [source="iso1234,iso5678"]
+      == Terms and Definitions
+
+     INPUT
+            <?xml version="1.0" encoding="UTF-8"?>
+       <iso-standard xmlns="http://riboseinc.com/isoxml">
+       <bibdata type="article">
+         <title>
+           <title-main language="en" format="text/plain"/>
+         </title>
+         <title>
+           <title-main language="fr" format="text/plain"/>
+         </title>
+         <docidentifier>
+           <project-number/>
+         </docidentifier>
+         <contributor>
+           <role type="author"/>
+           <organization>
+             <name>ISO</name>
+           </organization>
+         </contributor>
+         <contributor>
+           <role type="publisher"/>
+           <organization>
+             <name>ISO</name>
+           </organization>
+         </contributor>
+         <language/>
+         <script>Latn</script>
+         <status>
+           <stage>60</stage>
+           <substage>60</substage>
+         </status>
+         <copyright>
+           <from>2018</from>
+           <owner>
+             <organization>
+               <name>ISO</name>
+             </organization>
+           </owner>
+         </copyright>
+         <editorialgroup>
+           <technical-committee/>
+           <subcommittee/>
+           <workgroup/>
+         </editorialgroup>
+       </bibdata><version/>
+       <sections>
+         <terms id="_" obligation="normative">
+         <title>Terms and Definitions</title>
+         <source type="inline" bibitemid="iso1234" citeas=""/>
+         <source type="inline" bibitemid="iso5678" citeas=""/>
+       </terms>
+       </sections>
+       </iso-standard>
+     OUTPUT
+    end
 
 end
