8f867aafc66cec49841ebced55b1a0ff55b4ed4a
Nick Nicholas <opoudjis@optusnet.com.au>
2018-04-19 22:12:30 +1000

    rspec fix

--- spec/asciidoctor-iso/base_spec.rb
+++ spec/asciidoctor-iso/base_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Asciidoctor::ISO do
 <sections/>
 </iso-standard>
     OUTPUT
-    expect(File.exist?("spec/examples/rice.doc")).to be true
+    expect(File.exist?("test.doc")).to be true
   end
 
   it "converts a blank document" do
