1b2a7441b4a8cdca49ca2befe1dd4270fa8f3db5
Nick Nicholas <opoudjis@optusnet.com.au>
2019-07-10 22:26:20 +1000

    spec

--- spec/asciidoctor-iso/macros_spec.rb
+++ spec/asciidoctor-iso/macros_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Asciidoctor::ISO do
       deprecated:[term1]
       domain:[term1]
     INPUT
-            #{BLANK_HDR}
+    #{BLANK_HDR}
        <sections>
          <admitted>term1</admitted>
        <deprecates>term1</deprecates>
@@ -18,94 +18,4 @@ RSpec.describe Asciidoctor::ISO do
     OUTPUT
   end
 
-  it "processes the PlantUML macro" do
-    expect(strip_guid(Asciidoctor.convert(<<~"INPUT", backend: :iso, header_footer: true))).to be_equivalent_to <<~"OUTPUT"
-      #{ASCIIDOC_BLANK_HDR}
-      
-      [plantuml]
-      ....
-      @startuml
-      Alice -> Bob: Authentication Request
-      Bob --> Alice: Authentication Response
-
-      Alice -> Bob: Another authentication Request
-      Alice <-- Bob: another authentication Response
-      @enduml
-      ....
-
-      [plantuml]
-      ....
-      Alice -> Bob: Authentication Request
-      Bob --> Alice: Authentication Response
-
-      Alice -> Bob: Another authentication Request
-      Alice <-- Bob: another authentication Response
-      ....
-    INPUT
-       #{BLANK_HDR}
-       <sections><figure id="_">
-  <image src="plantuml/20.png" id="_" imagetype="PNG" height="auto" width="auto"/>
-</figure>
-<figure id="_">
-  <image src="plantuml/29.png" id="_" imagetype="PNG" height="auto" width="auto"/>
-</figure></sections>
-
-       </iso-standard>
-    OUTPUT
-  end
-
-  it "processes the PlantUML macro with PlantUML disabled" do
-    mock_plantuml_disabled
-    expect { Asciidoctor.convert(<<~"INPUT", backend: :iso, header_footer: true) }.to output(%r{PlantUML not installed}).to_stderr
-      #{ASCIIDOC_BLANK_HDR}
-
-      [plantuml]
-      ....
-      @startuml
-      Alice -> Bob: Authentication Request
-      Bob --> Alice: Authentication Response
-
-      Alice -> Bob: Another authentication Request
-      Alice <-- Bob: another authentication Response
-      @enduml
-      ....
-    INPUT
-
-    mock_plantuml_disabled
-    expect(strip_guid(Asciidoctor.convert(<<~"INPUT", backend: :iso, header_footer: true))).to be_equivalent_to <<~"OUTPUT"
-      #{ASCIIDOC_BLANK_HDR}
-
-      [plantuml]
-      ....
-      @startuml
-      Alice -> Bob: Authentication Request
-      Bob --> Alice: Authentication Response
-
-      Alice -> Bob: Another authentication Request
-      Alice <-- Bob: another authentication Response
-      @enduml
-      ....
-    INPUT
-       #{BLANK_HDR}
-       <sections>
-         <sourcecode lang="plantuml" id="_">@startuml
-Alice -&gt; Bob: Authentication Request
-Bob --&gt; Alice: Authentication Response
-
-Alice -&gt; Bob: Another authentication Request
-Alice &lt;-- Bob: another authentication Response
-@enduml</sourcecode>
-        </sourcecode>
-       </iso-standard>
-    OUTPUT
-  end
-
-
-  private
-
-  def mock_plantuml_disabled
-    expect(Asciidoctor::Standoc::PlantUMLBlockMacroBackend).to receive(:plantuml_installed?) do
-      false
-    end
-  end
 end
