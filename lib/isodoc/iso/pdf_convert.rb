09ab3285ac79ca7cf34328afe6d367b6386d4e4b
Nick Nicholas <opoudjis@gmail.com>
2020-02-11 23:24:52 +1100

    pdf convert

---
+++ lib/isodoc/iso/pdf_convert.rb
@@ -0,0 +1,27 @@
+require_relative "base_convert"
+require "isodoc"
+
+module IsoDoc
+  module Iso
+
+    # A {Converter} implementation that generates HTML output, and a document
+    # schema encapsulation of the document for validation
+    #
+    class PdfConvert < IsoDoc::XslfoPdfConvert
+      def initialize(options)
+        @libdir = File.dirname(__FILE__)
+        super
+      end
+
+      def convert(filename, file = nil, debug = false)
+        file = File.read(filename, encoding: "utf-8") if file.nil?
+        docxml, outname_html, dir = convert_init(file, filename, debug)
+        FileUtils.rm_rf dir
+        ::Metanorma::Output::XslfoPdf.new.convert(
+          filename, outname_html + ".pdf",
+          File.join(@libdir, "iso.international-standard.xsl"))
+      end
+    end
+  end
+end
+
