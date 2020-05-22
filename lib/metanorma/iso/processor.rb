cf91a123ae46f9b63680e28dfe6b8796308a58d9
Nick Nicholas <opoudjis@gmail.com>
2020-05-23 01:16:02 +1000

    fonts_used config: https://github.com/metanorma/metanorma-ogc/issues/145

--- lib/metanorma/iso/processor.rb
+++ lib/metanorma/iso/processor.rb
@@ -3,7 +3,12 @@ require "metanorma/processor"
 module Metanorma
   module Iso
     def self.fonts_used
-      ["Cambria", "Times New Roman", "Cambria Math", "HanSans", "Courier New"]
+      {
+        html: ["Cambria", "Times New Roman", "Cambria Math", "HanSans", "Courier New"],
+        html_alt: ["Cambria", "Times New Roman", "Cambria Math", "HanSans", "Courier New"],
+        doc: ["Cambria", "Times New Roman", "Cambria Math", "HanSans", "Courier New"],
+        pdf: ["Cambria", "Times New Roman", "Cambria Math", "HanSans", "Courier New"],
+      }
     end
 
     class Processor < Metanorma::Processor
