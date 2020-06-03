083a9c3fc9082d8382d710000a70667ab043f513
Nick Nicholas <opoudjis@gmail.com>
2020-06-03 21:42:11 +1000

    add PDF stylesheet for amendments, corrigenda

--- lib/isodoc/iso/pdf_convert.rb
+++ lib/isodoc/iso/pdf_convert.rb
@@ -13,6 +13,14 @@ module IsoDoc
         super
       end
 
+      def pdf_stylesheet(docxml)
+        case doctype = docxml&.at(ns("//bibdata/ext/doctype"))&.text
+        when "amendment", "technical-corrigendum" then "itu.recommendation-annex.xsl"
+        else
+          "iso.international-standard.xsl"
+        end
+      end
+
       def convert(filename, file = nil, debug = false)
         file = File.read(filename, encoding: "utf-8") if file.nil?
         docxml, outname_html, dir = convert_init(file, filename, debug)
@@ -23,8 +31,7 @@ module IsoDoc
         end
         FileUtils.rm_rf dir
         ::Metanorma::Output::XslfoPdf.new.convert(
-          filename, outname_html + ".pdf",
-          File.join(@libdir, "iso.international-standard.xsl"))
+          filename, outname_html + ".pdf", File.join(@libdir, pdf_stylesheet(docxml))
       end
     end
   end
