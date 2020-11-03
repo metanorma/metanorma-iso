d68049abbbcde38f875c9a812620a04c3765d271
Nick Nicholas <opoudjis@gmail.com>
2020-11-03 23:19:34 +1100

    handling of unpublished ISO citations: https://github.com/metanorma/metanorma-iso/issues/402

--- spec/isodoc/ref_spec.rb
+++ spec/isodoc/ref_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe IsoDoc do
       <abbreviation>ISO</abbreviation>
     </organization>
   </contributor>
-  <note format="text/plain" reference="1" type="ISO DATE">Under preparation. (Stage at the time of publication ISO/DIS 16634)</note>
+  <note format="text/plain" reference="1" type="Unpublished-Status">Under preparation. (Stage at the time of publication ISO/DIS 16634)</note>
   <extent type="part">
   <referenceFrom>all</referenceFrom>
   </extent>
@@ -175,7 +175,7 @@ RSpec.describe IsoDoc do
                    <abbreviation>ISO</abbreviation>
                  </organization>
                </contributor>
-               <note format='text/plain' reference='1' type='ISO DATE'>Under preparation. (Stage at the time of publication ISO/DIS 16634)</note>
+               <note format='text/plain' reference='1' type='Unpublished-Status'>Under preparation. (Stage at the time of publication ISO/DIS 16634)</note>
                <extent type='part'>
                  <referenceFrom>all</referenceFrom>
                </extent>
