9123773856dca9ed2d8f10fc6e07f8be697f068e
Nick Nicholas <opoudjis@gmail.com>
2021-01-19 23:09:59 +1100

    rspec

--- spec/asciidoctor-iso/validate_spec.rb
+++ spec/asciidoctor-iso/validate_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Asciidoctor::ISO do
       expect do
         Metanorma::Compile
           .new
-          .compile("spec/assets/xref_error.adoc", type: "iso")
+          .compile("spec/assets/xref_error.adoc", type: "iso", agree_to_terms: true)
       end.to(change { File.exist?("spec/assets/xref_error.err") }
               .from(false).to(true))
     end
