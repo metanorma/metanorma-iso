ea744a5683b59c7fbfcfcc1804e2d4f9beb65c13
Nick Nicholas <opoudjis@gmail.com>
2021-07-09 20:29:05 +1000

    adopt mnconvert: https://github.com/metanorma/metanorma-standoc/issues/492

--- lib/isodoc/iso/sts_convert.rb
+++ lib/isodoc/iso/sts_convert.rb
@@ -1,9 +1,8 @@
 require "isodoc"
-require "mn2sts"
+require "mnconvert"
 
 module IsoDoc
   module Iso
-
     class StsConvert < IsoDoc::XslfoPdfConvert
       def initialize(_options) # rubocop:disable Lint/MissingSuper
         @libdir = File.dirname(__FILE__)
@@ -14,14 +13,14 @@ module IsoDoc
       def convert(in_fname, file = nil, debug = false, out_fname = nil)
         file = File.read(in_fname, encoding: "utf-8") if file.nil?
         _docxml, filename, dir = convert_init(file, in_fname, debug)
-        unless /\.xml$/.match?(in_fname)
+        /\.xml$/.match?(in_fname) or
           in_fname = Tempfile.open([filename, ".xml"], encoding: "utf-8") do |f|
             f.write file
             f.path
           end
-        end
         FileUtils.rm_rf dir
-        Mn2sts.convert(in_fname, out_fname || "#{filename}.#{@suffix}")
+        Mnconvert.convert(in_fname, out_fname || "#{filename}.#{@suffix}",
+                          MnConvert::InputFormat::MN)
       end
     end
   end
