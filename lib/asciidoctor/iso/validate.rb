da173f2e663138fdd7d038a600cbe9626dbb79e8
Nick Nicholas <opoudjis@gmail.com>
2021-07-14 02:30:50 +1000

    correct detection of start/end of sentence text for text validation: https://github.com/metanorma/metanorma-iso/issues/519

--- lib/asciidoctor/iso/validate.rb
+++ lib/asciidoctor/iso/validate.rb
@@ -32,7 +32,7 @@ module Asciidoctor
         root.xpath("//xref").each do |t|
           preceding = t.at("./preceding-sibling::text()[last()]")
           next unless !preceding.nil? &&
-            /\b(see| refer to)\s*$/mi.match(preceding)
+            /\b(see| refer to)\s*\Z/mi.match(preceding)
 
           (target = root.at("//*[@id = '#{t['target']}']")) || next
           if target&.at("./ancestor-or-self::*[@obligation = 'normative']")
@@ -46,7 +46,7 @@ module Asciidoctor
       def see_erefs_validate(root)
         root.xpath("//eref").each do |t|
           prec = t.at("./preceding-sibling::text()[last()]")
-          next unless !prec.nil? && /\b(see|refer to)\s*$/mi.match(prec)
+          next unless !prec.nil? && /\b(see|refer to)\s*\Z/mi.match(prec)
 
           unless target = root.at("//*[@id = '#{t['bibitemid']}']")
             @log.add("Bibliography", t,
@@ -80,9 +80,9 @@ module Asciidoctor
         xmldoc.xpath("//term").each do |t|
           para = t.at("./definition") || return
           term = t.at("./preferred").text
-          termdef_warn(para.text, /^(the|a)\b/i, t, term,
+          termdef_warn(para.text, /\A(the|a)\b/i, t, term,
                        "term definition starts with article")
-          termdef_warn(para.text, /\.$/i, t, term,
+          termdef_warn(para.text, /\.\Z/i, t, term,
                        "term definition ends with period")
         end
         cited_term_style(xmldoc)
