df8c1f7f12e6e9bef1903d8ad419543b1a22d5c7
Nick Nicholas <opoudjis@gmail.com>
2021-09-17 22:25:20 +1000

    rspec

--- spec/asciidoctor/base_spec.rb
+++ spec/asciidoctor/base_spec.rb
@@ -5,60 +5,6 @@ RSpec.describe Asciidoctor::ISO do
     expect(Metanorma::ISO::VERSION).not_to be nil
   end
 
-  it "processes a blank document" do
-    input = <<~INPUT
-      #{ASCIIDOC_BLANK_HDR}
-    INPUT
-    output = <<~OUTPUT
-      #{BLANK_HDR}
-        <sections/>
-      </iso-standard>
-    OUTPUT
-    expect(xmlpp(Asciidoctor.convert(input, *OPTIONS)))
-      .to be_equivalent_to xmlpp(output)
-  end
-
-  it "converts a blank document" do
-    input = <<~INPUT
-      = Document title
-      Author
-      :docfile: test.adoc
-      :novalid:
-      :no-isobib:
-    INPUT
-    output = <<~OUTPUT
-      #{BLANK_HDR}
-        <sections/>
-      </iso-standard>
-    OUTPUT
-    expect(xmlpp(Asciidoctor.convert(input, *OPTIONS)))
-      .to be_equivalent_to xmlpp(output)
-    expect(File.exist?("test_alt.html")).to be true
-    expect(File.exist?("test.html")).to be true
-    expect(File.exist?("test.doc")).to be true
-    expect(File.exist?("test.pdf")).to be true
-    expect(File.exist?("htmlstyle.css")).to be false
-  end
-
-  it "converts a blank document in French" do
-    input = <<~INPUT
-      = Document title
-      Author
-      :docfile: test.adoc
-      :novalid:
-      :no-isobib:
-      :no-pdf:
-      :language: fr
-    INPUT
-    output = <<~OUTPUT
-      #{BLANK_HDR_FR}
-        <sections/>
-      </iso-standard>
-    OUTPUT
-    expect(xmlpp(Asciidoctor.convert(input, *OPTIONS)))
-      .to be_equivalent_to xmlpp(output)
-  end
-
   it "processes default metadata" do
     output = Asciidoctor.convert(<<~"INPUT", *OPTIONS)
       = Document title
---
+++ spec/asciidoctor/blank_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+RSpec.describe Asciidoctor::ISO do
+  it "processes a blank document" do
+    input = <<~INPUT
+      #{ASCIIDOC_BLANK_HDR}
+    INPUT
+    output = <<~OUTPUT
+      #{BLANK_HDR}
+        <sections/>
+      </iso-standard>
+    OUTPUT
+    expect(xmlpp(Asciidoctor.convert(input, *OPTIONS)))
+      .to be_equivalent_to xmlpp(output)
+  end
+
+  it "converts a blank document" do
+    input = <<~INPUT
+      = Document title
+      Author
+      :docfile: test.adoc
+      :novalid:
+      :no-isobib:
+    INPUT
+    output = <<~OUTPUT
+      #{BLANK_HDR}
+        <sections/>
+      </iso-standard>
+    OUTPUT
+    expect(xmlpp(Asciidoctor.convert(input, *OPTIONS)))
+      .to be_equivalent_to xmlpp(output)
+    expect(File.exist?("test_alt.html")).to be true
+    expect(File.exist?("test.html")).to be true
+    expect(File.exist?("test.doc")).to be true
+    expect(File.exist?("test.pdf")).to be true
+    expect(File.exist?("htmlstyle.css")).to be false
+  end
+end
