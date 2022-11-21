9ddcc803a977470e65e35824b11ae1100afad3f8
Nick Nicholas <opoudjis@gmail.com>
2022-11-22 01:33:00 +1100

    refactor eref_localities1: https://github.com/metanorma/metanorma-iso/issues/836

--- lib/isodoc/iso/isosts_convert.rb
+++ lib/isodoc/iso/isosts_convert.rb
@@ -13,14 +13,19 @@ module IsoDoc
         @suffix = "isosts.xml"
       end
 
-      def convert(input_fname, file = nil, debug = false, output_fname = nil)
-        file = File.read(input_fname, encoding: "utf-8") if file.nil?
-        _, fname, dir = convert_init(file, input_fname, debug)
+      def get_input_fname(input_fname)
         /\.xml$/.match(input_fname) or
           input_fname = Tempfile.open([fname, ".xml"], encoding: "utf-8") do |f|
             f.write file
             f.path
           end
+        input_fname
+      end
+
+      def convert(input_fname, file = nil, debug = false, output_fname = nil)
+        file = File.read(input_fname, encoding: "utf-8") if file.nil?
+        _, fname, dir = convert_init(file, input_fname, debug)
+        input_fname = get_input_fname(input_fname)
         FileUtils.rm_rf dir
         MnConvert.convert(input_fname,
                           {
