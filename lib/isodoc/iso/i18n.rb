d9a2cb890c45fd0443baaf69a67fa4910e99f438
Nick Nicholas <opoudjis@gmail.com>
2023-03-20 03:11:25 +1100

    Japanese i18n file for bridging isodoc and metanorma-jis

--- lib/isodoc/iso/i18n.rb
+++ lib/isodoc/iso/i18n.rb
@@ -5,10 +5,14 @@ module IsoDoc
         YAML.load_file(File.join(File.dirname(__FILE__), fname))
       end
 
+      # all gems inheriting from ISO need to be represented here,
+      # even if the file is empty
+      # e.g. JA for metanorma-jis
       def load_yaml1(lang, script)
         y = if lang == "en" then load_file("i18n-en.yaml")
             elsif lang == "fr" then load_file("i18n-fr.yaml")
             elsif lang == "ru" then load_file("i18n-ru.yaml")
+            elsif lang == "ja" then load_file("i18n-ja.yaml")
             elsif lang == "zh" && script == "Hans"
               load_file("i18n-zh-Hans.yaml")
             else load_file("i18n-en.yaml")
