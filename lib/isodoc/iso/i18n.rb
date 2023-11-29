11488437c97777d8657f5fc4a23910ea9d83c755
Nick Nicholas <opoudjis@gmail.com>
2023-11-30 00:31:39 +1100

    refactor i18n yaml to prevent overwriting parent non-English YAML with local English file: https://github.com/metanorma/isodoc/issues/549

--- lib/isodoc/iso/i18n.rb
+++ lib/isodoc/iso/i18n.rb
@@ -6,18 +6,8 @@ module IsoDoc
           {}
       end
 
-      # all gems inheriting from ISO need to be represented here,
-      # even if the file is empty
-      # e.g. JA for metanorma-jis
       def load_yaml1(lang, script)
-        y = if lang == "en" then load_file("i18n-en.yaml")
-            elsif lang == "fr" then load_file("i18n-fr.yaml")
-            elsif lang == "ru" then load_file("i18n-ru.yaml")
-            elsif lang == "ja" then load_file("i18n-ja.yaml")
-            elsif lang == "zh" && script == "Hans"
-              load_file("i18n-zh-Hans.yaml")
-            else load_file("i18n-en.yaml")
-            end
+        y = load_file("i18n-#{yaml_lang(lang, script)}.yaml")
         super.deep_merge(y)
       end
     end
