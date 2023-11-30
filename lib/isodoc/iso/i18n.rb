4ecf54200909f49af87164004ebff80e265a8a13
Nick Nicholas <opoudjis@gmail.com>
2023-11-30 20:54:58 +1100

    debug: https://github.com/metanorma/isodoc/issues/549

--- lib/isodoc/iso/i18n.rb
+++ lib/isodoc/iso/i18n.rb
@@ -6,9 +6,12 @@ module IsoDoc
         File.exist?(f) ? YAML.load_file(f) : {}
       end
 
+      alias :local_load_file :load_file
+
       def load_yaml1(lang, script)
-        y = load_file("i18n-#{yaml_lang(lang, script)}.yaml")
-        y.empty? ? load_file("i18n-en.yaml").merge(super) : super.deep_merge(y)
+        y = local_load_file("i18n-#{yaml_lang(lang, script)}.yaml")
+        y.empty? and return local_load_file("i18n-en.yaml").deep_merge(super)
+        super.deep_merge(y)
       end
     end
   end
