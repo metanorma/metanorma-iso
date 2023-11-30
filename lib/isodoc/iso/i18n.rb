1b3f173f13e8dd3712c652ab0ec1f10ff3415aa7
Nick Nicholas <opoudjis@gmail.com>
2023-11-30 21:09:33 +1100

    debug: https://github.com/metanorma/isodoc/issues/549

--- lib/isodoc/iso/i18n.rb
+++ lib/isodoc/iso/i18n.rb
@@ -1,16 +1,16 @@
 module IsoDoc
   module Iso
     class I18n < IsoDoc::I18n
-      def load_file(fname)
+      # name iso_load_file instead of load_file
+      # so that child flavours cannot inherit it and override it
+      def iso_load_file(fname)
         f = File.join(File.dirname(__FILE__), fname)
         File.exist?(f) ? YAML.load_file(f) : {}
       end
 
-      alias :local_load_file :load_file
-
       def load_yaml1(lang, script)
-        y = local_load_file("i18n-#{yaml_lang(lang, script)}.yaml")
-        y.empty? and return local_load_file("i18n-en.yaml").deep_merge(super)
+        y = iso_load_file("i18n-#{yaml_lang(lang, script)}.yaml")
+        y.empty? and return iso_load_file("i18n-en.yaml").deep_merge(super)
         super.deep_merge(y)
       end
     end
