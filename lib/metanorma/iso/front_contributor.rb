1021c6586bd8f0965c41976b359f7077dd5f5330
Nick Nicholas <opoudjis@gmail.com>
2025-08-05 22:14:50 +1000

    move committee contributor code to standoc: https://github.com/metanorma/metanorma-iec/issues/191

--- lib/metanorma/iso/front_contributor.rb
+++ lib/metanorma/iso/front_contributor.rb
@@ -15,7 +15,8 @@ module Metanorma
         org_contributor(node, xml,
                         { source: ["publisher", "pub"], role: "author",
                           default: default_publisher })
-        committee_contributors(node, xml, false, default_publisher)
+        committee_contributors(node, xml, default_publisher,
+                               { approval: false })
       end
 
       def org_organization(node, xml, org)
@@ -25,23 +26,23 @@ module Metanorma
         end
       end
 
-      def committee_contributors(node, xml, approval, agency)
-        t = metadata_approval_committee_types(approval ? node : nil)
+      def committee_contributors(node, xml, agency, opt)
+        t = metadata_approval_committee_types(opt[:approval] ? node : nil)
         v = t.first
         if node.attr("#{v}-number")
           node.attr(v) or node.set_attr(v, "")
-          o = committee_contrib_org_prep(node, v, approval, agency)
+          o = committee_contrib_org_prep(node, v, agency, opt)
           o[:groups] = t
-          o[:approval] = approval
+          o[:approval] = opt[:approval]
           org_contributor(node, xml, o)
         end
-        approval or committee_contributors_approval(node, xml, agency)
+        opt[:approval] or committee_contributors_approval(node, xml, agency)
       end
 
-      def committee_contrib_org_prep(node, type, approval, agency)
+      def committee_contrib_org_prep(node, type, agency, opt)
         agency_arr, agency_abbrev =
           committee_org_prep_agency(node, type, agency, [], [])
-        { source: [type], role: approval ? "authorizer" : "author",
+        { source: [type], role: opt[:approval] ? "authorizer" : "author",
           default_org: false, committee: true, agency: agency_arr,
           agency_abbrev:,
           desc: type.sub(/^approval-/, "").tr("-", " ").capitalize }.compact
@@ -60,133 +61,37 @@ module Metanorma
         [agency_arr, agency_abbr]
       end
 
-      def org_attrs_complex_parse(node, opts, source)
-        i = 1
-        suffix = ""
-        ret = []
-        while node.attr("#{source}-number#{suffix}") || node.attr("#{source}#{suffix}")
-          ret << extract_org_attrs_complex(node, opts, source, suffix)
-          i += 1
-          suffix = "_#{i}"
-        end
-        ret
-      end
-
       def committee_contributors_approval(node, xml, agency)
         o = { name: agency, role: "authorizer", default_org: false,
               desc: "Agency", committee: false }
         org_contributor(node, xml, o)
       end
 
-      def extract_org_attrs_complex(node, opts, source, suffix)
-        n = node.attr("#{source}-number#{suffix}")
-        t = committee_abbrev(node.attr("#{source}-type#{suffix}"), n, source)
-        super.merge(ident: t).compact
-      end
-
-      def contrib_committee_build(xml, agency, committee)
-        if name = org_abbrev.invert[agency]
-          committee[:agency_abbrev] = agency
-          agency = name
-        end
-        xml.name agency
-        s = committee
-        loop do
-          contrib_committee_subdiv(xml, s)
-          s = s[:subdiv] or break
-        end
-        abbr = committee[:agency_abbrev] and xml.abbreviation abbr
-        full_committee_id(xml.parent)
-      end
-
-      def contrib_committee_subdiv(xml, committee)
-        contributors_committees_filter_empty?(committee) and return
-        xml.subdivision **attr_code(type: committee[:desc]) do |o|
-          o.name committee[:name]
-          # s = committee[:subdiv] and contrib_committee_subdiv(o, s)
-          committee[:abbr] and o.abbreviation committee[:abbr]
-          committee[:ident] and o.identifier committee[:ident]
-        end
-      end
-
-      def full_committee_id(contrib)
-        ret = full_committee_agency_id(contrib)
-        ids = contrib.xpath("./subdivision").map { |x| x.at("./identifier")&.text }
-        ins = contrib.at("./subdivision/identifier") and
-          ins.next = "<identifier type='full'>#{ret}#{ids.join('/')}</identifier>"
-      end
-
-      def full_committee_agency_id(contrib)
-        agency = contrib.at("./abbreviation")&.text
-        ret = agency == default_publisher ? "" : "#{agency} "
-        /^\s+/.match?(ret) and ret = ""
-        ret
-      end
-
-      COMMITTEE_ABBREVS =
+      def committee_abbrevs
         { "technical-committee" => "TC", "subcommittee" => "SC",
-          "workgroup" => "WG" }.freeze
+          "workgroup" => "WG" }
+      end
 
-      def committee_abbrev(type, number, level)
+      def committee_ident(type, number, level)
         number.nil? || number.empty? and return
-        type ||= COMMITTEE_ABBREVS[level.sub(/^approval-/, "")]
+        type ||= committee_abbrevs[level.sub(/^approval-/, "")]
         type == "Other" and type = ""
         "#{type} #{number}".strip
       end
 
-      def org_attrs_parse(node, opts)
-        opts_orig = opts.dup
-        ret = []
-        ret << super&.map&.with_index do |x, i|
-          x.merge(agency: opts.dig(:agency, i),
-                  agency_abbrev: opts.dig(:agency_abbrev, i), abbr: opts[:abbr],
-                  committee: opts[:committee], default_org: opts[:default_org])
-        end
-        opts_orig[:groups]&.each_with_index do |g, i|
-          i.zero? and next
-          contributors_committees_pad_multiples(ret, node, g)
-          opts = committee_contrib_org_prep(node, g, opts_orig[:approval], nil)
-          ret << super
-        end
-        #ret = contributors_committees_filter_empty(ret)
-        #ret.first
-        ret = contributors_committees_nest1(ret)
-      end
-
-      # ensure there is subcommittee, workgroup -number_2, -number_3 etc
-      # to parse mutlple tech committees
-      def contributors_committees_pad_multiples(committees, node, group)
-        committees.each_with_index do |_r, j|
-          suffix = j.zero? ? "" : "_#{j + 1}"
-          node.attr("#{group}#{suffix}") or
-            node.set_attr("#{group}#{suffix}", "")
-          node.attr("#{group}-number#{suffix}") or
-            node.set_attr("#{group}-number#{suffix}", "")
-        end
-      end
-
       def contributors_committees_filter_empty?(committee)
-            committee[:name].empty? &&
-              (committee[:ident].nil? || %w(WG TC SC).include?(committee[:ident]))
-      end
-
-      def contributors_committees_nest1(committees)
-        committees.empty? and return committees
-        committees = committees.reverse
-        committees.each_with_index do |m, i|
-          i.zero? and next
-          m.each_with_index do |m1, j|
-            m1[:subdiv] = committees[i - 1][j]
-          end
-        end
-        committees[-1]
+        committee[:name].empty? &&
+          (committee[:ident].nil? || %w(WG TC
+                                        SC).include?(committee[:ident]))
       end
 
       def metadata_publisher(node, xml)
         super
         # approvals
-        committee_contributors(node, xml, true,
-                               node.attr("approval-agency") || default_publisher)
+        committee_contributors(
+          node, xml,
+          node.attr("approval-agency") || default_publisher, { approval: true }
+        )
       end
 
       def metadata_committee(node, xml)
@@ -239,11 +144,11 @@ module Metanorma
         suffix = ""
         while node.attr(compname + suffix)
           num = node.attr("#{compname}-number#{suffix}")
-                          name = node.attr(compname + suffix)
-                          (!num.nil? && !num.empty?) || (!name.nil? && !name.empty?) and
-          out.send compname.gsub(/-/, "_"), name,
-            **attr_code(number: num,
-                               type: node.attr("#{compname}-type#{suffix}"))
+          name = node.attr(compname + suffix)
+          (!num.nil? && !num.empty?) || (!name.nil? && !name.empty?) and
+            out.send compname.gsub(/-/, "_"), name,
+                     **attr_code(number: num,
+                                 type: node.attr("#{compname}-type#{suffix}"))
           i += 1
           suffix = "_#{i}"
         end
