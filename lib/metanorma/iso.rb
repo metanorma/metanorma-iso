2377d0857964ddc7185b51b33696b9af3c4eccc6
Ronald Tse <ronald.tse@ribose.com>
2026-08-09 15:43:03 +0800

    Add Metanorma::Iso.validate top-level shortcut
    Users can now write Metanorma::Iso.validate(xml) instead of
    Metanorma::Iso::API.validate(xml). Delegates with **opts so all
    keyword arguments pass through (profile, lang, doctype, etc.).
    
      report = Metanorma::Iso.validate(xml)
      report = Metanorma::Iso.validate(xml, profile: :publication)
      report = Metanorma::Iso.validate(xml, doctype: 'international-standard')
    
    207 specs green.

--- lib/metanorma/iso.rb
+++ lib/metanorma/iso.rb
@@ -4,5 +4,11 @@ module Metanorma
   module Iso
     autoload :Validation, "metanorma/iso/validation"
     autoload :API, "metanorma/iso/api"
+
+    # Convenience shortcut for Metanorma::Iso::API.validate.
+    # @see Metanorma::Iso::API.validate
+    def self.validate(xml, **opts)
+      API.validate(xml, **opts)
+    end
   end
 end
